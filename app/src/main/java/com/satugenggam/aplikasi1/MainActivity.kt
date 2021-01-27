17cd1afc6388085cb2571776693163509f2e32bb
Suzukaze1 <alvinmd68@gmail.com>
2021-01-27 11:40:37 +0700

    first commit

---
+++ app/src/main/java/com/satugenggam/aplikasi1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.satugenggam.aplikasi1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
