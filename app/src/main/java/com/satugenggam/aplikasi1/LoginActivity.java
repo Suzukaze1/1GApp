70a60b0cda8f7d0a03f128944dee6871cc99e97b
Suzukaze1 <alvinmd68@gmail.com>
2021-02-06 16:29:18 +0700

    first commit

--- app/src/main/java/com/satugenggam/aplikasi1/LoginActivity.java
+++ app/src/main/java/com/satugenggam/aplikasi1/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.satugenggam.aplikasi1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -13,6 +15,10 @@ public class LoginActivity extends AppCompatActivity {
 
     TabLayout tabLayout;
     ViewPager viewPager;
+    View mIndicator;
+    FrameLayout fl01;
+
+    private int indicatorWidth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +29,48 @@ public class LoginActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tab_layout);
         viewPager = findViewById(R.id.view_pager);
+        mIndicator = findViewById(R.id.indicator);
+        fl01 = findViewById(R.id.frame_layout01);
 
         viewPager.setOffscreenPageLimit(2);
         LoginAdapter adapter = new LoginAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
-        tabLayout.setTranslationY(300);
-        tabLayout.setAlpha(0);
-        tabLayout.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+        fl01.setTranslationY(300);
+        fl01.setAlpha(0);
+        fl01.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(800).start();
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                indicatorWidth = tabLayout.getWidth() / tabLayout.getTabCount();
+
+                FrameLayout.LayoutParams indicatorParams = (FrameLayout.LayoutParams) mIndicator.getLayoutParams();
+                indicatorParams.width = indicatorWidth;
+                mIndicator.setLayoutParams(indicatorParams);
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                FrameLayout.LayoutParams params = (FrameLayout.LayoutParams)mIndicator.getLayoutParams();
+
+                float translationOffset = (positionOffset+position) * indicatorWidth;
+                params.leftMargin = (int) translationOffset;
+                mIndicator.setLayoutParams(params);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
