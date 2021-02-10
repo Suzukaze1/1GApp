d81830086f040946bdf9661b5b331e5154b736d6
Suzukaze1 <alvinmd68@gmail.com>
2021-02-10 18:09:53 +0700

    first commit

---
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/viewmodel/view/dashboard/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.satugenggam.aplikasi1.ui.viewmodel.view.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.satugenggam.aplikasi1.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/viewmodel/view/profil/ShopEditActivity.java
@@ -0,0 +1,16 @@
+package com.satugenggam.aplikasi1.ui.viewmodel.view.profil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.satugenggam.aplikasi1.R;
+
+public class ShopEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_edit);
+    }
+}
