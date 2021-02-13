7c09c6fdb95f026b627fdc32c8d70ca0f84a12d2
Suzukaze1 <alvinmd68@gmail.com>
2021-02-13 14:53:08 +0700

    Shop Profile: - profile shop - edit profile shop - password change shop

---
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/viewmodel/view/profil/ShopChangePasswordActivity.java
@@ -0,0 +1,19 @@
+package com.satugenggam.aplikasi1.ui.viewmodel.view.profil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.satugenggam.aplikasi1.R;
+
+public class ShopChangePasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_change_password);
+
+        // Tombol Arah panah balik
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
