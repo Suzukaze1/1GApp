50fa1c64eb529dfee1eaad918e4595becf47c262
Suzukaze1 <alvinmd68@gmail.com>
2021-02-25 11:48:38 +0700

    Order : status in progress

--- app/src/main/java/com/satugenggam/aplikasi1/ui/view/dashboard/LayoutActivity.java
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/view/dashboard/LayoutActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.satugenggam.aplikasi1.R;
 
-public class DashboardActivity extends AppCompatActivity {
+public class LayoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_layout);
     }
 }
--- app/src/main/java/com/satugenggam/aplikasi1/ui/view/order/OrderActivity.java
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/view/order/OrderActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.google.android.material.tabs.TabLayout;
 import com.satugenggam.aplikasi1.LoginAdapter;
@@ -22,9 +23,15 @@ public class OrderActivity extends AppCompatActivity {
         tabLayout = findViewById(R.id.tab_layout_order);
         viewPager = findViewById(R.id.view_pager_order);
 
-        viewPager.setOffscreenPageLimit(2);
+        viewPager.setOffscreenPageLimit(7);
         OrderPagerFragment adapter = new OrderPagerFragment(this, getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.shop_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
