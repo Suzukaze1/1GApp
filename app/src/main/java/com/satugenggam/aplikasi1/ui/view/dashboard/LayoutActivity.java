dc06497a30cd8cbf48d0f01c4f2b12206bb5806c
Suzukaze1 <alvinmd68@gmail.com>
2021-02-27 13:26:33 +0700

    Order : status in progress

--- app/src/main/java/com/satugenggam/aplikasi1/ui/view/dashboard/LayoutActivity.java
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/view/dashboard/LayoutActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.Menu;
@@ -15,13 +17,16 @@ import android.widget.Button;
 
 import com.google.android.material.navigation.NavigationView;
 import com.satugenggam.aplikasi1.R;
+import com.satugenggam.aplikasi1.ui.view.order.BlankFragment;
+import com.satugenggam.aplikasi1.ui.view.order.OrderActivity;
 import com.satugenggam.aplikasi1.ui.view.order.OrderFragment;
 
 public class LayoutActivity extends AppCompatActivity {
 
+    private Fragment fragment = new DashboardFragment();
     ActionBarDrawerToggle mDrawableToggle;
     DrawerLayout mDrawerLayout;
-    Button btnOrder;
+    Button btnOrder, btnDashboard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,16 +34,28 @@ public class LayoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_layout);
 
         mDrawerLayout = findViewById(R.id.drawer_layout01);
+        btnDashboard = findViewById(R.id.btn_beranda);
         btnOrder = findViewById(R.id.btn_pesanan);
 
         mDrawableToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawerOpen, R.string.drawerClose);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container1, fragment).commit();
+
+        btnDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.closeDrawers();
+                Fragment fragment = new DashboardFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container1, fragment).commit();
+            }
+        });
+
         btnOrder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OrderFragment fragment = new OrderFragment();
-                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_container, fragment, "Fragment_TAG");
-                fragmentTransaction.commit();
+                mDrawerLayout.closeDrawers();
+                Fragment fragment = new OrderFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container1, fragment).commit();
             }
         });
     }
--- app/src/main/java/com/satugenggam/aplikasi1/ui/view/order/OrderFragment.java
+++ app/src/main/java/com/satugenggam/aplikasi1/ui/view/order/OrderFragment.java
@@ -2,6 +2,8 @@ package com.satugenggam.aplikasi1.ui.view.order;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -24,12 +26,20 @@ public class OrderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        tabLayout = container.findViewById(R.id.tab_layout_order);
-        viewPager = container.findViewById(R.id.view_pager_order);
-        viewPager.setOffscreenPageLimit(7);
-        viewPager.setAdapter(new OrderPagerFragment(getContext(), getChildFragmentManager()));
-        tabLayout.setupWithViewPager(viewPager);
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tabLayout = view.findViewById(R.id.tab_layout_order1);
+        viewPager = view.findViewById(R.id.view_pager_order1);
+
+        viewPager.setOffscreenPageLimit(4);
+        OrderPagerFragment adapter = new OrderPagerFragment(getContext(), getChildFragmentManager());
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
